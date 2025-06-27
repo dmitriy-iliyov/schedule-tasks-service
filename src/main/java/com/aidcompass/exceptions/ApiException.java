75afe8a49ece11f8db4de12a875faffa2637cf8f
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-06-27 21:08:59 +0200

    add(construcor in ApiException & login in ScheduleSystemRestClient)

--- src/main/java/com/aidcompass/exceptions/ApiException.java
+++ src/main/java/com/aidcompass/exceptions/ApiException.java
@@ -5,8 +5,15 @@ import com.aidcompass.exceptions.models.dto.ErrorDto;
 
 public class ApiException extends BaseInternalServiceException {
 
+    private final String message;
+
+
+    public ApiException(String message) {
+        this.message = message;
+    }
+
     @Override
     public ErrorDto getErrorDto() {
-        return new ErrorDto("api", "Api request produce exception!");
+        return new ErrorDto("api", "Api response: " + message);
     }
 }
--- src/main/java/com/aidcompass/rest_client/ScheduleSystemRestClient.java
+++ src/main/java/com/aidcompass/rest_client/ScheduleSystemRestClient.java
@@ -3,18 +3,52 @@ package com.aidcompass.rest_client;
 import com.aidcompass.exceptions.ApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
 public class ScheduleSystemRestClient {
 
-    private final RestClient restClient;
+    @Value("${service.name}")
+    private String serviceName;
 
+    @Value("${service.key}")
+    private String serviceKey;
+
+    private final RestClient restClient;
+    private final static String BEARER_TEMPLATE = "Bearer %s";
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loginAfterApplicationStart() {
+        login();
+    }
+
+    public String login() {
+        Map<String, String> credentials = Map.of(
+                "service_name", serviceName,
+                "service_key", serviceKey
+        );
+        try {
+            return restClient
+                    .post()
+                    .uri("/auth/login")
+                    .body(credentials)
+                    .retrieve()
+                    .body(String.class);
+        } catch (RestClientException e) {
+            log.error(e.getMessage());
+            throw new ApiException(e.getMessage());
+        }
+    }
 
     public List<Long> deleteIntervalsBatchBeforeWeakStart(int batchSize) {
         try {
@@ -24,11 +58,12 @@ public class ScheduleSystemRestClient {
                             .path("/intervals/past/batch")
                             .queryParam("batch_size", batchSize)
                             .build())
+//                    .header("Authorization", BEARER_TEMPLATE.formatted(jwt))
                     .retrieve()
                     .body(new ParameterizedTypeReference<List<Long>>() {});
         } catch (RestClientException e) {
             log.error(e.getMessage());
-            throw new ApiException();
+            throw new ApiException(e.getMessage());
         }
     }
 
@@ -44,7 +79,7 @@ public class ScheduleSystemRestClient {
                     .body(new ParameterizedTypeReference<List<Long>>() {});
         } catch (RestClientException e) {
             log.error(e.getMessage());
-            throw new ApiException();
+            throw new ApiException(e.getMessage());
         }
     }
 }
