e2cb1077c219fc48d6af98bcf31dcad74c64c30f
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-07-01 21:23:26 +0200

    add(batch notification)

--- src/main/java/com/aidcompass/services/ScheduleNotificationService.java
+++ src/main/java/com/aidcompass/services/ScheduleNotificationService.java
@@ -7,8 +7,10 @@ import com.aidcompass.models.enums.TaskStatus;
 import com.aidcompass.models.enums.TaskType;
 import com.aidcompass.repositories.ContinueFlagRepository;
 import com.aidcompass.repositories.TaskRepository;
+import com.aidcompass.rest_client.ScheduleSystemRestClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,25 +23,47 @@ public class ScheduleNotificationService {
 
     private final ContinueFlagRepository continueFlagRepository;
     private final TaskRepository taskRepository;
+    private final ScheduleSystemRestClient restClient;
 
 
+    @Scheduled(cron = "* */5 4-8 * * *")
     @Transactional
     public void notifyBatchBeforeAppointment() {
         ContinueFlagEntity continueFlagEntity = continueFlagRepository.findByType(TaskType.NOTIFY_BEFORE_APPOINTMENT)
                 .orElseThrow(ContinueFlagNotFoundByTypeException::new);
         if (continueFlagEntity.isShouldContinue()) {
             int batchSize = continueFlagEntity.getBatchSize();
+            int page = continueFlagEntity.getPage();
             TaskEntity taskEntity = new TaskEntity(
                     TaskType.NOTIFY_BEFORE_APPOINTMENT,
-                    TaskStatus.NOT_COMPLETED,
+                    TaskStatus.PROCESSED,
                     batchSize,
+                    page,
                     Instant.now(),
                     null
             );
-            taskRepository.save(taskEntity);
+            taskEntity = taskRepository.save(taskEntity);
 
-            //List<> appointments = appointmentRepository.
+            int nextPage = page + 1;
+
+            try {
+                boolean continueNotifying = restClient.notifyBatch(batchSize, page);
+                if (!continueNotifying) {
+                    continueFlagEntity.setShouldContinue(false);
+                }
+                taskEntity.setStatus(TaskStatus.COMPLETED);
+            } catch (Exception e) {
+                log.error("Error when notifying: {}", e.getMessage());
+                taskEntity.setStatus(TaskStatus.ERROR);
+            } finally {
+                taskEntity.setEnd(Instant.now());
+                taskEntity.setPage(nextPage);
+                taskRepository.save(taskEntity);
+                continueFlagEntity.setPage(nextPage);
+                continueFlagRepository.save(continueFlagEntity);
+            }
+            return;
         }
-        log.info("shouldContinue flag: {}", continueFlagEntity.isShouldContinue());
+        log.info("Task {} skipped: shouldContinue=false", TaskType.NOTIFY_BEFORE_APPOINTMENT);
     }
 }
